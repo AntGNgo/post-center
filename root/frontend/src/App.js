50bb5318f616541a7c1a347a762a5d1a9e5dde57
AntGNgo <anthony.g.ngo@gmail.com>
2020-11-28 12:56:09 -0800

    Finished 1st Rev of style on Home page

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,11 +1,12 @@
 import Navigation from './components/Navigation'
+import Post from './components/Post'
 
 function App() {
   return (
-    <div className="container mx-auto px-2 bg-white">
+    <div className="container mx-auto px-2 bg-white h-screen">
 
       <Navigation />
-      <Posts />
+      <Post />
     </div>
   );
 }
---
+++ root/frontend/src/components/Post.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Post = () => {
+    return(
+        <div className="">
+            <h1 className="text-xl mt-5">First Post</h1>
+            <h2 className='text-sm'>11/28/2020</h2>
+            <p className='mt-4 px-6 pb-4 leading-6 border-b border-grey-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente natus harum neque praesentium explicabo placeat quam nulla expedita, velit nihil commodi minus pariatur. Quibusdam, iure beatae nemo numquam tenetur dolores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto possimus perferendis natus nemo voluptates labore, omnis distinctio? Quisquam quas assumenda reprehenderit nihil. Architecto sit laboriosam voluptatibus possimus ipsa animi ullam consectetur corrupti! Officia quasi aperiam aspernatur soluta reiciendis illo adipisci ipsum aut repudiandae, quam facere. Ipsum voluptatum nemo officiis nulla.</p>
+        </div>
+    )
+}
+
+export default Post
